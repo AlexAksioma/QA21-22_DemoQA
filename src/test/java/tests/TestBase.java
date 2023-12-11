9f8ec169f0127ce892c465ab3707e41afdb86081
AlexAksioma <alexmedqwerty@gmail.com>
2023-12-11 19:47:42 +0200

    added AppManager, tests

---
+++ src/test/java/tests/TestBase.java
@@ -0,0 +1,17 @@
+package tests;
+
+import manager.AppManager;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class TestBase implements AppManager {
+
+    @BeforeSuite
+    public void setup(){
+        init();
+    }
+    @AfterSuite
+    public void tearDown(){
+        //stop();
+    }
+}
