29a24f7b4172cdef27b9a30af83daa98a9774a55
AlexAksioma <alexmedqwerty@gmail.com>
2023-12-18 20:16:27 +0200

    added student dto, student form test

--- src/main/java/manager/HelperStudent.java
+++ src/main/java/manager/HelperStudent.java
@@ -1,5 +1,27 @@
 package manager;
 
+import models.StudentDTO;
+import org.openqa.selenium.By;
+
 public interface HelperStudent extends HelperBase{
+    By buttonElements = By.xpath("//div[@class='card mt-4 top-card']");
+    By buttonForms = By.xpath("//div[text()='Forms']");
+    By buttonPracticeForm = By.xpath("//span[text()='Practice Form']");
+
+    By fieldFistName = By.id("firstName");
+    By fieldLastName = By.id("lastName");
+    By fieldEmail = By.id("userEmail");
+
+    default void selectPracticeForm(){
+        clickBase(buttonElements);
+        pause(5);
+        clickBase(buttonForms);
+        clickBase(buttonPracticeForm);
+    }
+    default void fillStudentForm(StudentDTO student) {
+        typeBase(fieldFistName, student.getFirstName());
+        typeBase(fieldLastName, student.getLastName());
+        typeBase(fieldEmail, student.getEmail());
+    }
 
 }
